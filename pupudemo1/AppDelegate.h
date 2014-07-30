f78ba1aab3a203014ad702423c0a1eab53b5c033
safecapitalgithub <paulyip.safecapital@gmail.com>
2014-07-30 11:34:59 +0800

    Initial Commit

---
+++ pupudemo1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pupudemo1
+//
+//  Created by Paul Yip on 30/7/14.
+//  Copyright (c) 2014 safecapital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ pupudemo1Tests/pupudemo1Tests.m
@@ -0,0 +1,34 @@
+//
+//  pupudemo1Tests.m
+//  pupudemo1Tests
+//
+//  Created by Paul Yip on 30/7/14.
+//  Copyright (c) 2014 safecapital. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pupudemo1Tests : XCTestCase
+
+@end
+
+@implementation pupudemo1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
